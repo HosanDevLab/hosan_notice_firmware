eef1e96bbba70e02f818e3f277b448e074e9c264
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-09-28 00:34:09 +0900

    아니 왜안됨

--- config.example.h
+++ config.example.h
@@ -1,2 +1,14 @@
 #define WIFI_SSID "ssid"
 #define WIFI_PASSWORD "password1234"
+#define FIREBASE_SERVICE_ACCOUNT_FILE "service_account.json"
+#define FIREBASE_PROJECT_ID "hosan-notice"
+#define FIREBASE_API_KEY "api key"
+#define FIREBASE_USER_EMAIL "12345@hosan.hs.kr"
+#define FIREBASE_USER_PASSWORD "password"
+
+#define USE_EAP false
+#define EAP_SSID "ssid"
+#define EAP_USERNAME "username"
+#define EAP_PASSWORD "password"
+#define FIREBASE_DOC_ID "1b"
+#define GENDER 0 // Man: 0, Woman: 1
--- hosan_notice_firmware.ino
+++ hosan_notice_firmware.ino
@@ -1,7 +1,11 @@
+#pragma GCC optimize ("-O2")
 #include <ESP8266WiFi.h>
+#include "wpa2_enterprise.h"
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Scheduler.h>
+#include <Firebase_ESP_Client.h>
+// #include "addons/TokenHelper.h"
 #include "config.h"
 #define TRIG 2 // D4
 #define ECHO 16 // D0
@@ -17,14 +21,31 @@
 WiFiServer server(80);
 LiquidCrystal_I2C lcd(0x27,16,2);
 
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
 int menu = 0;
+float sonarDistance = 0;
+float sonarPercent = 0;
+
+void lcdPrintDistance()
+{
+  lcd.setCursor(0, 1);
+  lcd.print(LCD_BLANK);
+  lcd.setCursor(0, 1);
+  lcd.print(sonarDistance);
+  lcd.print("cm (");
+  lcd.print(sonarPercent);
+  lcd.print("%)");
+}
 
 class sonarTask : public Task {
 protected:
   void setup() {}
 
   void loop() {       
-    float distance = 0;
     float duration = 0;
     digitalWrite(TRIG,LOW);
     delayMicroseconds(2);
@@ -32,18 +53,23 @@ protected:
     delayMicroseconds(10);
     digitalWrite(TRIG,LOW);
     duration = pulseIn(ECHO,HIGH);
-    distance = duration * 17/1000;
+    sonarDistance = duration * 17/1000;
+    sonarPercent = duration * 105/1000;
 
-    Serial.println(distance);
-    
+    Serial.println(sonarDistance);
 
-    if (distance >= 11)
+    if (menu == 0)
     {
-      digitalWrite(LED_R, HIGH);
-      digitalWrite(LED_G ,LOW);
+      lcdPrintDistance();
+    }
+
+    if (sonarDistance >= 11)
+    {
+      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_G ,HIGH);
       digitalWrite(LED_B, LOW);
     }
-    else if (distance >= 8)
+    else if (sonarDistance >= 8)
     {
       digitalWrite(LED_R, LOW);
       digitalWrite(LED_G ,LOW);
@@ -51,8 +77,8 @@ protected:
     }
     else
     {
-      digitalWrite(LED_R, LOW);
-      digitalWrite(LED_G ,HIGH);
+      digitalWrite(LED_R, HIGH);
+      digitalWrite(LED_G ,LOW);
       digitalWrite(LED_B, LOW);
     }
 
@@ -67,13 +93,9 @@ protected:
   void loop() {
     delay(100);
 
-    if (!digitalRead(BUTTON))
-    {
-      return;
-      
-    }
+    if (!digitalRead(BUTTON)) return;
 
-    if (menu < 1) menu++;
+    if (menu < 2) menu++;
     else menu = 0; 
 
     lcd.setCursor(0, 1);
@@ -82,12 +104,17 @@ protected:
     switch (menu)
     {
       case 0:
-        lcd.setCursor(0, 1);
-        lcd.print(WiFi.localIP());
+        lcdPrintDistance();
         break;
       case 1:
         lcd.setCursor(0, 1);
+        if (WiFi.status() == WL_CONNECTED) lcd.print(WiFi.localIP());
+        else lcd.print("OFFLINE MODE");
+        break;
+      case 2:
+        lcd.setCursor(0, 1);
         lcd.print(VERSION);
+        break;
     }
 
     while (digitalRead(BUTTON));
@@ -102,10 +129,43 @@ protected:
   void loop() {
     delay(5000);
 
-    Serial.println("aa");
     yield();
   }
 } wifi_task;
+
+class dataPostTask : public Task {
+protected:
+  void setup () {}
+
+  void loop() {
+    Serial.print(Firebase.ready());
+    if (Firebase.ready())
+    {
+      FirebaseJson content;
+      
+      content.clear();
+      if (GENDER == 0)
+      {
+        content.set("fields/man/integerValue", String(sonarPercent).c_str());  
+      } else {
+        content.set("fields/woman/integerValue", String(sonarPercent).c_str());
+      }
+
+      Serial.print("Update a document... ");
+
+      char p1[] = "toilet_paper/";
+      char p2[] = FIREBASE_DOC_ID;
+
+      if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", strcat(p1, p2), content.raw(), GENDER == 0 ? "man" : "woman"))
+        Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+      else
+        Serial.println(fbdo.errorReason());
+      delay(60000);
+    }
+    else delay(1000);
+    yield();
+  }
+} data_post_task;
 
 void setup() {
   pinMode(TRIG, OUTPUT); // D3
@@ -140,44 +200,107 @@ void setup() {
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
+  Serial.println(USE_EAP ? EAP_SSID : WIFI_SSID);
+  if (USE_EAP) Serial.println("USING EAP");
 
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 0);
   lcd.print("Connecting WIFI");
- 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  lcd.setCursor(0, 1);
+  lcd.print("button to skip");
+
+  if (USE_EAP)
+  {
+    wifi_set_opmode(STATION_MODE);
+    
+    // Configure SSID
+    struct station_config wifi_config;
+    memset(&wifi_config, 0, sizeof(wifi_config));
+    strcpy((char *)wifi_config.ssid, EAP_SSID);
+    wifi_station_set_config(&wifi_config);
+    
+    // DO NOT use authentication using certificates
+    wifi_station_clear_cert_key();
+    wifi_station_clear_enterprise_ca_cert();
+    
+    // Authenticate using username/password
+    wifi_station_set_wpa2_enterprise_auth(1);
+    wifi_station_set_enterprise_identity((uint8 *)EAP_USERNAME, strlen(EAP_USERNAME));
+    wifi_station_set_enterprise_username((uint8 *)EAP_USERNAME, strlen(EAP_USERNAME));
+    wifi_station_set_enterprise_password((uint8 *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+    
+    // Connect
+    wifi_station_connect();
+  } else {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  }
 
   while (WiFi.status() != WL_CONNECTED && !digitalRead(BUTTON)) {
     delay(500);
     Serial.print(".");
   }
-
-  digitalWrite(14, 1);
-  Serial.println("");
-
-  delay(1000);
-
-  // Start the server
-  lcd.setCursor(0, 1);
-  lcd.print("Starting Server");
+  Serial.println();
   
-  server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
+  // LED red when WIFI connected
+  digitalWrite(14, 1);
+
+  lcd.setCursor(0, 0);
+  lcd.print("  Hosan Notice  ");
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    // Connecting to Firebase
+    lcd.setCursor(0, 1);
+    lcd.print("Connecting DB...");
+  
+    // config.service_account.json.path = FIREBASE_SERVICE_ACCOUNT_FILE;
+    // config.service_account.json.storage_type = mem_storage_type_flash;
+    // config.token_status_callback = tokenStatusCallback;
+    // config.max_token_generation_retry = 5;
+    // auth.token.uid = "";
+    config.api_key = FIREBASE_API_KEY;
+
+    auth.user.email = FIREBASE_USER_EMAIL;
+    auth.user.password = FIREBASE_USER_PASSWORD;
+
+    Firebase.reconnectWiFi(true);
+    Firebase.begin(&config, &auth);
+
+    /*
+    while (!Firebase.ready()) {
+      delay(500);
+      Serial.print(".");
+    }
+    */
+  
+    delay(1000);
+    
+  
+    // Start the server
+    lcd.setCursor(0, 1);
+    lcd.print("Starting Server");
+    
+    server.begin();
+    Serial.println("Server started");
+    
+   
+    // Print the IP address
+    Serial.print("Use this URL to connect: ");
+    Serial.print("http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("/");
+  }
 
   lcd.setCursor(0, 1);
   lcd.print(LCD_BLANK);
   lcd.setCursor(0, 1);
-  lcd.print(WiFi.localIP());
+  if (WiFi.status() == WL_CONNECTED) lcd.print(WiFi.localIP());
+  else lcd.print("OFFLINE MODE");
 
+  // Begin background tasks
   Scheduler.start(&button_task);
   Scheduler.start(&wifi_task);
   Scheduler.start(&sonar_task);
+  Scheduler.start(&data_post_task);
   Scheduler.begin();
 }
 
